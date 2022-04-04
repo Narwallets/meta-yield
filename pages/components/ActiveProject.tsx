c11de03e303e7f4934916005f6de0b4e5d174ab1
saguero <sergiogaguero@gmail.com>
2022-04-04 18:19:27 -0300

    changes in homepage

--- pages/components/ActiveProject.tsx
+++ pages/components/ActiveProject.tsx
@@ -15,6 +15,9 @@ import {
   useBreakpointValue,
   SimpleGrid,
   AvatarBadge,
+  Square,
+  Flex,
+  Circle,
 } from "@chakra-ui/react";
 import { Card } from "./Card";
 import Image from "next/image";
@@ -28,102 +31,100 @@ export const ActiveProject = (props: { projects: ProjectProps[] }) => {
   const iconColor = useColorModeValue("indigo.500", "indigo.200");
   const router = useRouter();
   return (
-    <Box
-      mx="auto"
-      px={{ base: "4", md: "8", lg: "12" }}
-      py={{ base: "6", md: "8", lg: "12" }}
-      as="section"
-      id="latest"
-    >
-      <Stack spacing="10">
-        <Text fontSize="4xl" lineHeight="10" fontWeight="bold">
-          Current Project
-        </Text>
-        <Card>
-          <SimpleGrid columns={3} spacing={10}>
-            <Box>
+    <Box as="section" pt={{ base: "50", md: "100" }} pb={{ base: "12", md: "24" }}>
+      <Text fontSize="4xl"  lineHeight="10" fontWeight="bold">
+        Current Project
+      </Text>
+      <Card p={0} mt={10}>
+        <Flex>
+          <Box borderRadius={useBreakpointValue({ base: "md", md: "xl" })} minW={400} minH={310} backgroundColor={'black'}>
+            <Flex  alignItems={'center'} height={310}>
               <Image
                 src={project.imageUrl}
                 alt="project"
                 width="400"
-                height="310"
-                layout="responsive"
-                objectFit="cover"
+                height={'100%'}
+                layout={'fixed'}
               />
-            </Box>
-            <Box>
-              <Stack
-                spacing={{ base: "1", md: "2" }}
-                direction={{ base: "column", md: "row" }}
-              >
-                <Image
-                  src={project.avatarUrl}
-                  alt="project"
-                  width="60"
-                  height="60"
-                />
-              </Stack>
-              <Stack
-                spacing={{ base: "1", md: "2" }}
-                direction={{ base: "column", md: "row" }}
-              >
-                <Text as="h2" fontWeight="bold" fontSize="xl">
-                  {project.name}
+            </Flex>
+          </Box>
+          <Box mr="10" ml="10">
+            <Stack
+              spacing={{ base: "1", md: "2" }}
+              direction={{ base: "column", md: "row" }}
+            >
+              <Circle boxShadow='xl'  ml="-8" mb="2" >
+                <Circle m="2" overflow={'hidden'}>
+                  <Image
+                    src={project.avatarUrl}
+                    alt="project"
+                    width="60"
+                    height="60"
+                  />
+                </Circle>
+              </Circle>
+            </Stack>
+            <Stack
+              spacing={{ base: "1", md: "2" }}
+              direction={{ base: "column", md: "row" }}
+            >
+              <Text as="h2" fontWeight="bold" fontSize="24px">
+                {project.name}
+              </Text>
+              <CircleWavyCheck size={24} />
+            </Stack>
+            <Text mt="2">{project.description}</Text>
+            <Wrap
+              shouldWrapChildren
+              mt="5"
+              color={useColorModeValue("gray.600", "gray.300")}
+            >
+              {project.tags && project.tags.map((tag) => (
+                <Tag key={tag} color="inherit" px="3">
+                  {tag}
+                </Tag>
+              ))}
+            </Wrap>
+          </Box>
+          <Box>
+            <Stack spacing="10">
+              <VStack align="flex-start" spacing="1 ">
+                <Text fontSize="sm" fontWeight="subtle">
+                  {" "}
+                  Time Left
+                </Text>
+                <Text fontSize="md" color="emphasized">
+                  14 days
+                </Text>
+              </VStack>
+              <VStack align="flex-start" spacing="1">
+                <Text fontSize="sm" fontWeight="subtle">
+                  TOKENOMICS
+                </Text>
+                <Text fontSize="md" color="emphasized">
+                  $12,3456,678 raised
+                </Text>
+              </VStack>
+              <Stack align="flex-start" spacing="4">
+                <Text color="emphasized" fontSize="md">
+                  3,526 supporters
                 </Text>
                 {project.verified && <CircleWavyCheck size={24} />}
               </Stack>
-              <Text mt="2">{project.description}</Text>
-              <Wrap
-                shouldWrapChildren
-                mt="5"
-                color={useColorModeValue("gray.600", "gray.300")}
-              >
-                {project.tags &&
-                  project.tags.map((tag) => (
-                    <Tag key={tag} color="inherit" px="3">
-                      {tag}
-                    </Tag>
-                  ))}
-              </Wrap>
-            </Box>
-            <Box>
-              <Stack spacing="10">
-                <VStack align="flex-start" spacing="1 ">
-                  <Text fontSize="sm" fontWeight="subtle">
-                    {" "}
-                    Time Left
-                  </Text>
-                  <Text fontSize="md" color="emphasized">
-                    14 days
-                  </Text>
-                </VStack>
-                <VStack align="flex-start" spacing="1">
-                  <Text fontSize="sm" fontWeight="subtle">
-                    TOKENOMICS
-                  </Text>
-                  <Text fontSize="md" color="emphasized">
-                    $12,3456,678 raised
-                  </Text>
-                </VStack>
-                <Stack align="flex-start" spacing="4">
-                  <Text color="emphasized" fontSize="md">
-                    3,526 supporters
-                  </Text>
-                </Stack>
-                <Stack align="flex-start" spacing="4">
-                  <Button
-                    colorScheme="indigo"
-                    rightIcon={<CaretRight size={20} />}
-                    onClick={() => router.push(`/project/${project.id}`)}
-                  >
-                    Fund Now
-                  </Button>
-                </Stack>
+              <Stack align="flex-start" spacing="4">
+                <Button
+                  w={'100%'}
+                  colorScheme="indigo"
+                  rightIcon={<CaretRight size={20} />}
+                  onClick={() => router.push(`/project/${project.id}`)}
+                >
+                  Fund Now
+                </Button>
               </Stack>
-            </Box>
-          </SimpleGrid>
-        </Card>
-      </Stack>
+            </Stack>
+          </Box>
+        </Flex>
+      </Card>
     </Box>
   );
 };
