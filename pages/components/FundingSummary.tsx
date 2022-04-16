9c5fbcd717daf138f9d8361d7bb08a5b105c67f3
Manzanal, Leandro <lmanzanal@aclaratech.com>
2022-04-16 16:01:08 -0300

    stnear amount validation

--- pages/components/FundingSummary.tsx
+++ pages/components/FundingSummary.tsx
@@ -30,6 +30,8 @@ import {
 } from "../../lib/near";
 import { useStore } from "./../../stores/wallet";
 import { yoctoToStNear } from "../../lib/util";
+import { useFormik } from "formik";
+import fundKickstarterSchema from "../../validation/fundSupporterSchema";
 
 const FundingSummary = (props: { id: any }) => {
   const kickstarter_id: number = props.id;
@@ -37,7 +39,7 @@ const FundingSummary = (props: { id: any }) => {
   const { wallet } = useStore();
 
   const { isLoading, data: project } = useGetProjectDetails(kickstarter_id);
-  const [amountToFund, setAmountToFund] = useState<number>(0);
+  const [amount, setamount] = useState<number>(0);
   const [fundingNeeded, setFundingNeeded] = useState<number | undefined>(
     undefined
   );
@@ -48,15 +50,11 @@ const FundingSummary = (props: { id: any }) => {
   const [currentFundingGoal, setCurrentFundingGoal] =
     useState<KickstarterGoalProps>();
   const [estimatedRewards, setEstimatedRewards] = useState<number>(0);
-  const handleChange = (event: any) => setAmountToFund(event.target.value);
+  const handleChange = (event: any) => setamount(event.target.value);
 
   const onMaxClick = async (event: any) =>
-    setAmountToFund(await getBalance(wallet!));
+    formik.setFieldValue("amount", await getBalance(wallet!));
 
-  const fund = async (event: any) => {
-    const result = fundToKickstarter(wallet!, kickstarter_id, amountToFund);
-    router.push(`/project/success/${project.id}`);
-  };
   const getCurrentFundingGoal = () => {
     const [currentFundingGoal] = project.kickstarter.goals.filter(
       (g: KickstarterGoalProps) =>
@@ -67,6 +65,26 @@ const FundingSummary = (props: { id: any }) => {
     }
     return currentFundingGoal;
   };
+  const initialValues: any = {
+    amount: 0,
+    balance: 0,
+  };
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: fundKickstarterSchema,
+    validateOnMount: true,
+    enableReinitialize: true,
+    validateOnBlur: true,
+    validateOnChange: true,
+    onSubmit: async (values: any) => {
+      const result = await fundToKickstarter(
+        wallet!,
+        kickstarter_id,
+        values.amount
+      );
+      router.push(`/project/success/${project.id}`);
+    },
+  });
 
   useEffect(() => {
     if (project) {
@@ -87,11 +105,18 @@ const FundingSummary = (props: { id: any }) => {
     if (currentFundingGoal) {
       const tokenAwardPerStnear: string = currentFundingGoal.tokens_to_release;
       setEstimatedRewards(
-        yoctoToStNear(parseInt(tokenAwardPerStnear)) * amountToFund
+        yoctoToStNear(parseInt(tokenAwardPerStnear)) * amount
       );
     }
-  }, [amountToFund]);
+  }, [amount]);
 
+  useEffect(() => {
+    async function setBalance() {
+      formik.setFieldValue("balance", await getBalance(wallet!));
+    }
+
+    setBalance();
+  }, []);
 
   if (isLoading && !project) return <>Loading</>;
   return (
@@ -172,9 +197,13 @@ const FundingSummary = (props: { id: any }) => {
                     <Alien size={32} color="indigo" />
                   </InputLeftAddon>
                   <Input
+                    id="amount"
+                    name="amount"
                     placeholder="0"
-                    value={amountToFund}
-                    onChange={handleChange}
+                    value={formik.values.amount}
+                    onPaste={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
                   />
                   <InputRightElement width="4.5rem">
                     <Button h="1.75rem" size="sm" onClick={onMaxClick}>
@@ -247,8 +276,15 @@ const FundingSummary = (props: { id: any }) => {
             </Card>
 
             <Stack align="center">
-              <Button colorScheme="blue" isFullWidth size="lg" onClick={fund}>
-                Invest {amountToFund} stNEAR from {wallet?.getAccountId()}
+              <Button
+                colorScheme="blue"
+                isFullWidth
+                size="lg"
+                onClick={(e: any) => formik.handleSubmit(e)}
+                disabled={!formik.isValid}
+              >
+                Invest {formik.values.amount} stNEAR from{" "}
+                {wallet?.getAccountId()}
               </Button>
             </Stack>
           </Stack>
