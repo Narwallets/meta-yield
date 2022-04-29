5c05a1756be4997164c639dd8918b0f0fa15e5e2
Manzanal, Leandro <lmanzanal@aclaratech.com>
2022-04-29 12:40:34 -0300

    hardcode ENV until dev environment is created

--- lib/near.ts
+++ lib/near.ts
@@ -34,7 +34,7 @@ import { ExecutionError } from "near-api-js/lib/providers/provider";
 export const CONTRACT_ID = process.env.CONTRACT_ID;
 export const METAPOOL_CONTRACT_ID = process.env.METAPOOL_CONTRACT_ID;
 export const gas = new BN("70000000000000");
-const nearConfig = getConfig(process.env.NODE_ENV || "development");
+const nearConfig = getConfig("development");
 const provider = new providers.JsonRpcProvider({ url: nearConfig.nodeUrl });
 
 export const getNearConfig = () => {
