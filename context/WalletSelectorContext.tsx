5ce186c84b648cd21a462fbb165362d2418742c7
Leandro Manzanal <leomanzanal@gmail.com>
2022-08-18 16:39:52 -0300

    update wallets

--- context/WalletSelectorContext.tsx
+++ context/WalletSelectorContext.tsx
@@ -12,16 +12,10 @@ import {
   NearWalletParams,
   setupNearWallet,
 } from "@near-wallet-selector/near-wallet";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupSender } from "@near-wallet-selector/sender";
 import { setupMathWallet } from "@near-wallet-selector/math-wallet";
-import { setupNightly } from "@near-wallet-selector/nightly";
-import { setupLedger } from "@near-wallet-selector/ledger";
-import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
-import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
 import { CONTRACT_ID, METAPOOL_CONTRACT_ID, NETWORK_ID } from "../lib/near";
 import { getConfig } from "../config";
- declare global {
+declare global {
   interface Window {
     selector: WalletSelector;
     modal: WalletSelectorModal;
@@ -44,6 +38,7 @@ enum Wallets {
   Nightly = "nightly",
   WalletConnect = "walletconnect",
   NightlyConnect = "nightlyconnect",
+  Ledger = "ledger",
 }
 
 const WalletSelectorContext =
@@ -55,101 +50,36 @@ export const WalletSelectorContextProvider: React.FC = ({ children }) => {
   const [selector, setSelector] = useState<WalletSelector | null>(null);
   const [modal, setModal] = useState<WalletSelectorModal | null>(null);
   const [accounts, setAccounts] = useState<Array<AccountState>>([]);
-  // comma separated enable wallets
-  const WALLET_SELECTOR_ENABLE_WALLETS =
-    process.env.NEXT_PUBLIC_WALLET_SELECTOR_ENABLE_WALLETS;
-  const DEFAULT_ENABLE_WALLETS = ["near", "mynearwallet", "math"];
-  const setupNearWalletCustom = () => {
-    return async (options: any) => {
-      const wallet = await setupMyNearWallet({
-        walletUrl: nearConfig.walletUrl
-      })(options);
+  const DEFAULT_ENABLE_WALLETS = ["near", "math"];
 
-      if (!wallet) {
-        return null;
+  const setupWallets = () => {
+    let modules: any[] = [];
+    const enableWallets = DEFAULT_ENABLE_WALLETS;
+    enableWallets.forEach((w: string) => {
+      switch (w) {
+        case Wallets.Near: {
+          modules.push(
+            setupNearWallet({
+              walletUrl: nearConfig.walletUrl,
+              iconUrl: "./assets/near-wallet-icon.png",
+            })
+          );
+          break;
+        }
+        case Wallets.Math: {
+          modules.push(setupMathWallet());
+          break;
+        }
       }
-
-      return {
-        ...wallet,
-        id: "near-wallet",
-        metadata: {
-          ...wallet.metadata,
-          name: "NEAR Wallet",
-          description: null,
-          iconUrl: "./assets/near-wallet-icon.png",
-          deprecated: false,
-          available: true,
-        },
-      };
-    };
+    });
+    return modules;
   };
 
-      // Setup Wallet Selector wallets based on WALLET_SELECTOR_ENABLE_WALLETS enn variable
-      const setupWallets = () => {
-        let modules: any[] = [];
-        const enableWallets = WALLET_SELECTOR_ENABLE_WALLETS
-          ? WALLET_SELECTOR_ENABLE_WALLETS.split(",")
-          : DEFAULT_ENABLE_WALLETS;
-        enableWallets.forEach((w: string) => {
-          switch (w) {
-            case Wallets.Near: {
-              modules.push(setupNearWalletCustom());
-              break;
-            }
-            case Wallets.MyNearWallet: {
-              modules.push(setupMyNearWallet());
-              break;
-            }
-            case Wallets.Math: {
-              modules.push(setupMathWallet());
-              break;
-            }
-            case Wallets.Nightly: {
-              modules.push(setupNightly());
-              break;
-            }
-            case Wallets.NightlyConnect: {
-              const setup = setupNightlyConnect({
-                url: "wss://ncproxy.nightly.app/app",
-                appMetadata: {
-                  additionalInfo: "",
-                  application: "NEAR Wallet Selector",
-                  description: "Meta Yield app used by NEAR Wallet Selector",
-                  icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
-                },
-              });
-              modules.push(setup);
-              break;
-            }
-            case Wallets.WalletConnect: {
-              const setup = 
-              setupWalletConnect({
-                  projectId: "c4f79cc...",
-                  metadata: {
-                    name: "NEAR Wallet Selector",
-                    description: "Example dApp used by NEAR Wallet Selector",
-                    url: "https://github.com/near/wallet-selector",
-                    icons: ["https://avatars.githubusercontent.com/u/37784886"],
-                  },
-                });
-              modules.push(setup);
-              break;
-            }
-            case Wallets.Sender: {
-              modules.push(setupSender());
-              break;
-            }
-          }
-        });
-        return modules;
-      };
-
   const init = useCallback(async () => {
-    const modules = setupWallets();
     const _selector = await setupWalletSelector({
       network: NETWORK_ID as NetworkId,
       debug: true,
-      modules: modules
+      modules: setupWallets(),
     });
 
     const _modal = setupModal(_selector, { contractId: CONTRACT_ID || "" });
