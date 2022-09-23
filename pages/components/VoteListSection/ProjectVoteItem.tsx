ad0d25ffca5b7ea88e787007e6475b58eaafcdd3
Leandro Manzanal <leomanzanal@gmail.com>
2022-09-23 17:41:53 -0300

    show result of operation + clear the vote field

--- pages/components/VoteListSection/ProjectVoteItem.tsx
+++ pages/components/VoteListSection/ProjectVoteItem.tsx
@@ -8,11 +8,14 @@ import {
   HStack,
   VStack,
   Button,
+  useToast
 } from "@chakra-ui/react";
 import * as React from "react";
 import { getVotes, voteProject } from "../../../lib/near";
 import { ntoy, yton } from "../../../lib/util";
 import { useEffect, useState } from "react";
+import { FinalExecutionOutcome } from "near-api-js/lib/providers";
+import TxErrorHandler from "../TxErrorHandler";
 
 interface Props {
   project: any;
@@ -21,9 +24,23 @@ interface Props {
 const ProjectVoteItem = (props: Props) => {
   const { project } = props;
   const [votes, setVotes] = useState("0");
-
+  const [finalExecutionOutcome, setFinalExecutionOutcome] =   useState<FinalExecutionOutcome | null>(null);
+  const toast = useToast();
   const vote = (slug: string, amount?: any) => {
-    voteProject(slug, ntoy(1));
+    voteProject(slug, ntoy(1)).then((result)=> {
+      setVotes("0");
+      setFinalExecutionOutcome(result);
+    })  
+    .catch((error) => {
+      toast({
+        title: "Transaction error.",
+        description: error,
+        status: "error",
+        duration: 9000,
+        position: "top-right",
+        isClosable: true,
+      });
+    });
   };
 
   useEffect(() => {
@@ -36,6 +53,8 @@ const ProjectVoteItem = (props: Props) => {
   if (!props || !project) return <></>;
 
   return (
+    <>
+    <TxErrorHandler finalExecutionOutcome={finalExecutionOutcome} />
     <Flex boxShadow={"xl"} m={5} p={10} w={"100%"} direction={"row"}>
       <Image alt="logo" h={"95px"} src={project.avatarUrl}></Image>
       <Text ml={5} fontSize={"2xl"} fontWeight={600}>
@@ -52,6 +71,7 @@ const ProjectVoteItem = (props: Props) => {
         Vote
       </Button>
     </Flex>
+    </>
   );
 };
 
