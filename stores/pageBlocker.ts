5345f75e1b56c214448f0937268b6dddcb5b21a8
Leandro Manzanal <leomanzanal@gmail.com>
2022-10-10 18:06:15 -0300

    fix build error

--- stores/pageBlocker.ts
+++ stores/pageBlocker.ts
@@ -1,5 +1,5 @@
 import create from "zustand/vanilla";
-import { PageBlockerState } from "../pages/components/PageBlocker/pageblocker.types";
+import { PageBlockerState } from "@meta-pool-apps/meta-shared-components";
 
 export const blockerStore = create<PageBlockerState>(() => ({
   message: "Confirm this action in your wallet",
